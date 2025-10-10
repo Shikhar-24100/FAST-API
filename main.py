f6c96cfb314615c215add49b65c16bf6a02c3772
Shikhar Shrivastav <shrishikhar1709@gmail.com>
2025-10-10 13:53:45 +0530

    added patient-view endpoint

--- main.py
+++ main.py
@@ -26,3 +26,12 @@ def view():
 
     return data
 
+
+@app.get('/patient/{patient_id}')
+def view_patient(patient_id: str):
+    #load all patients
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {'error': 'patient not found'}
+
