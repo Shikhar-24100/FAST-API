7f7723accd65c73ae38fd92c7feed5aa6b494bb4
Shikhar Shrivastav <shrishikhar1709@gmail.com>
2025-10-19 19:52:59 +0530

    patient model creation

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI, Path, HTTPException, Query
 import json
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 
 app = FastAPI()
+
+class Patient(BaseModel):
+
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['P001'])]
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City where the patient is living')]
+    age: Annotated[int, Field(..., gt =0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., description='Height of the patient in metres')]
+    weight: Annotated[float, Field(..., description='Weight of the patient in kgs')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round((self.weight/self.height**2), 2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> float:
+        if self.bmi < 18.5:
+            return 'Underweight'
+        elif self.bmi < 25:
+            return 'Normal'
+        elif self.bmi < 30:
+            return 'Overweight'
+        else:
+            return 'Obese'
+
 
 def load_data():
     with open('patients.json', 'r') as f:
